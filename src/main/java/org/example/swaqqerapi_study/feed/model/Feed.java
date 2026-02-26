20bdcb82bea310fb282ab2ff712d2dfd585ff633
jdh <wjdehd555@gmail.com>
2026-02-26 17:47:25 +0900

    [Feat] 게시물 리스트 조회 기능 추가

---
+++ src/main/java/org/example/swaqqerapi_study/feed/model/Feed.java
@@ -0,0 +1,25 @@
+package org.example.swaqqerapi_study.feed.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.example.swaqqerapi_study.user.model.User;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Feed {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idx;
+    private String contents;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="user_idx")
+    private User user;
+
+}
