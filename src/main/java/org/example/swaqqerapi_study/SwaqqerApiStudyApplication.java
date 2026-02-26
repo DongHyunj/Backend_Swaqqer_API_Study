371d779bc7892bcf484716b78036ff3dce3de6a3
jdh <wjdehd555@gmail.com>
2026-02-26 15:06:27 +0900

    [ãInit] 프로젝트 초기 설정

---
+++ src/main/java/org/example/swaqqerapi_study/SwaqqerApiStudyApplication.java
@@ -0,0 +1,13 @@
+package org.example.swaqqerapi_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaqqerApiStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwaqqerApiStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/swaqqerapi_study/SwaqqerApiStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.swaqqerapi_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaqqerApiStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
