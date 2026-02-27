467ff89d961eb19c67891b37aa1a1f4b9e62e159
jdh <wjdehd555@gmail.com>
2026-02-27 17:31:29 +0900

    [Feat] 좋아요 스웨거 API 상세 설정

--- src/main/java/org/example/swaqqerapi_study/likes/LikesController.java
+++ src/main/java/org/example/swaqqerapi_study/likes/LikesController.java
@@ -1,5 +1,7 @@
 package org.example.swaqqerapi_study.likes;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.example.swaqqerapi_study.common.model.BaseResponse;
 import org.example.swaqqerapi_study.user.model.AuthUserDetails;
@@ -13,9 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/likes")
 @RestController
 @RequiredArgsConstructor
+@Tag(name="좋아요 기능")
 public class LikesController {
     private final LikesService likesService;
 
+    @Operation(summary = "좋아요 등록 기능", description = "좋아요를 한번 누르면 좋아요 등록, 한번 더 누르면 절회")
     @GetMapping("/{feedIdx}")
     public ResponseEntity likes(@AuthenticationPrincipal AuthUserDetails user, @PathVariable Long feedIdx) {
         likesService.likes(user, feedIdx);
