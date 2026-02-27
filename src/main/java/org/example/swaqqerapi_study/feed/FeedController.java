606aa549b023cc59b92ac5deb41edcfee03fe51e
jdh <wjdehd555@gmail.com>
2026-02-27 14:33:36 +0900

    [Feat] 목록 조회할 때 현재 로그인 한 사용자의 좋아요 여부 표시

--- src/main/java/org/example/swaqqerapi_study/feed/FeedController.java
+++ src/main/java/org/example/swaqqerapi_study/feed/FeedController.java
@@ -2,7 +2,9 @@ package org.example.swaqqerapi_study.feed;
 
 import lombok.RequiredArgsConstructor;
 import org.example.swaqqerapi_study.feed.model.FeedDto;
+import org.example.swaqqerapi_study.user.model.AuthUserDetails;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,8 +18,8 @@ public class FeedController {
     private final FeedSerivce feedSerivce;
 
     @GetMapping("/list")
-    public ResponseEntity list() {
-        List<FeedDto.ListRes> result = feedSerivce.list();
+    public ResponseEntity list(@AuthenticationPrincipal AuthUserDetails user) {
+        List<FeedDto.ListRes> result = feedSerivce.list(user);
 
         return ResponseEntity.ok(result);
     }
--- src/main/java/org/example/swaqqerapi_study/feed/model/FeedDto.java
+++ src/main/java/org/example/swaqqerapi_study/feed/model/FeedDto.java
@@ -2,6 +2,7 @@ package org.example.swaqqerapi_study.feed.model;
 
 import lombok.Builder;
 import lombok.Getter;
+import org.example.swaqqerapi_study.user.model.AuthUserDetails;
 
 public class FeedDto {
 
@@ -12,13 +13,17 @@ public class FeedDto {
         private String contents;
         private String writer;
         private Long likesCount;
+        private boolean isLikes;
 
-        public static ListRes from(Feed entity) {
+        public static ListRes from(Feed entity, AuthUserDetails user) {
             return ListRes.builder()
                     .idx(entity.getIdx())
                     .contents(entity.getContents())
                     .writer(entity.getUser().getName())
                     .likesCount(Long.valueOf(entity.getLikesList().size()))
+                    .isLikes(entity.getLikesList().stream().anyMatch(
+                            likes -> likes.getUser().getIdx() == user.getIdx()
+                    ))
                     .build();
         }
     }
